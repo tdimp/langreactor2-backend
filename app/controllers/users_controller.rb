1ffe5f9d0b48ec21197e9316d00fbe1ab4723875
Thomas Dimperio <twdimperio@gmail.com>
2022-08-22 19:36:47 -0600

    Build out UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,45 @@
 class UsersController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :handle_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_not_found_response
+  
+  def index
+    users = User.all
+    render json: users
+  end
+  
+  def show
+    user = User.find(params[:id])
+    render json: user
+  end
+
+  def create
+    user = User.create!(user_params)
+    render json: user, status: :created  
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update!(user_params)
+    render json: user
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    head :no_content
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password, :password_confirmation, :primary_language)
+  end
+
+  def handle_unprocessable_entity_response(exception)
+    render json: { errors: exception.record.errors }, status: :unprocessable_entity
+  end
+
+  def handle_not_found_response
+    render json: { error: "User not found." }, status: :not_found
+  end
 end
