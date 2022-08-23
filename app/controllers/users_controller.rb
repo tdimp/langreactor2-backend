8804b5009570a0fa47cbd827d56eb7c75491a78c
Thomas Dimperio <twdimperio@gmail.com>
2022-08-23 16:23:43 -0600

    Add authentication for DecksController, login/logout routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,12 @@ class UsersController < ApplicationController
   end
   
   def show
-    user = User.find(params[:id])
-    render json: user
+    user = User.find_by(id: session[:user_id])
+    if user
+      render json: user
+    else
+      render json: { error: "Not authorized" }, status: :unauthorized
+    end
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   resources :decks
   resources :users
 
+  # user authentication and logout routes
+  post '/login', to: "sessions#create"
+  delete '/logout', to: "sessions#destroy"
+
 end
