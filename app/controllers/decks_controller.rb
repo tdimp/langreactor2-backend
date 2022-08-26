2295d579ca4acaa4abe66996b3679ee25e43b608
Thomas Dimperio <twdimperio@gmail.com>
2022-08-26 16:02:57 -0600

    Add /.byebug_history to .gitignore, add seed data, add index and show functionality to DecksController

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,9 +1,33 @@
 class DecksController < ApplicationController
-  before_action :authorize
+  #before_action :authorize
+  rescue_from ActiveRecord::RecordInvalid, with: :handle_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_not_found_response
+
+  def index
+    #byebug
+    user = User.find(1)
+    render json: user.decks
+  end
+
+  def show
+    deck = Deck.find(params[:id])
+    render json: deck.cards
+  end
+
+
 
   private
 
   def authorize
     return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
   end
+
+  def handle_unprocessable_entity_response(exception)
+    render json: { error: exception.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def handle_not_found_response
+    render json: { error: "Deck not found" }, status: :not_found
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
 User.create(username: "testuser", password_digest: "password", primary_language: "English")
 
 Deck.create(name: "Spanish Deck", user_id: 1)
+Deck.create(name: "German Deck", user_id: 1)
 
+Card.create!(foreign_language: "Spanish", primary_lang_txt: "English Test", foreign_lang_txt: "Prueba de Español", img_url: "test")
 Card.create!(foreign_language: "Spanish", primary_lang_txt: "English Test", foreign_lang_txt: "Prueba de Español", img_url: "test")
 
 DeckCard.create(deck_id: 1, card_id: 1)
+DeckCard.create(deck_id: 1, card_id: 2)
