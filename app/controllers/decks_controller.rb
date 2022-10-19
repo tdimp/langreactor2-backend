b3b6439246d693db0b54442605420bffe160f8c4
Thomas Dimperio <twdimperio@gmail.com>
2022-10-19 16:07:37 -0600

    Add create deck funtionality to DecksController

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -10,8 +10,17 @@ class DecksController < ApplicationController
     render json: deck.cards unless deck.user_id != session[:user_id]
   end
 
+  def create
+    deck = Deck.create!(deck_params)
+    render json: deck
+  end
+
 
   private
+
+  def deck_params
+    params.permit(:name, :user_id)
+  end
 
   
 end
