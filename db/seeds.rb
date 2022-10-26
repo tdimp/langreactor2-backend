a13230ce3d3fadb57abd3e1027417b791c18b94e
Thomas Dimperio <twdimperio@gmail.com>
2022-10-26 11:28:05 -0600

    Change seed data to reflect ActiveRecord associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
-User.create(username: "testuser", password_digest: BCrypt::Password.create("password"), primary_language: "English")
-User.create(username: "user2", password_digest: BCrypt::Password.create("password"), primary_language: "Spanish")
+user_1 = User.create(username: "testuser", password_digest: BCrypt::Password.create("password"), primary_language: "English")
+user_2 = User.create(username: "user2", password_digest: BCrypt::Password.create("password"), primary_language: "Spanish")
 
-Deck.create(name: "Spanish Deck", user_id: 1)
-Deck.create(name: "German Deck", user_id: 1)
-Deck.create(name: "English Deck", user_id: 2)
+deck_1 = user_1.decks.create(name: "Spanish Deck")
+deck_2 = user_1.decks.create(name: "German Deck")
+deck_3 = user_2.decks.create(name: "English Deck")
 
-Card.create!(foreign_language: "Spanish", primary_lang_txt: "English Test", foreign_lang_txt: "Prueba de Español", img_url: "https://istvbox.com/wp-content/uploads/2018/11/test_356816.png", user_id: 1)
-Card.create!(foreign_language: "Spanish", primary_lang_txt: "Test", foreign_lang_txt: "Prueba de Español", img_url: "test", user_id: 1)
-Card.create!(foreign_language: "German", primary_lang_txt: "German Test", foreign_lang_txt: "Test", img_url: "test", user_id: 1)
+deck_1.cards.create!(foreign_language: "Spanish", primary_lang_txt: "English Test", foreign_lang_txt: "Prueba de Español", img_url: "https://istvbox.com/wp-content/uploads/2018/11/test_356816.png", user_id: 1)
+deck_1.cards.create!(foreign_language: "Spanish", primary_lang_txt: "Test", foreign_lang_txt: "Prueba de Español", img_url: "test", user_id: 1)
+deck_2.cards.create!(foreign_language: "German", primary_lang_txt: "German Test", foreign_lang_txt: "Test", img_url: "test", user_id: 1)
 
-DeckCard.create(deck_id: 1, card_id: 1)
-DeckCard.create(deck_id: 1, card_id: 2)
-DeckCard.create(deck_id: 2, card_id: 3)
+join_card = deck_1.cards.create!(foreign_language: "Test", primary_lang_txt: "Join Test", foreign_lang_txt: "TEST TEST TEST", img_url: "test", user_id: 1)
+deck_2.cards << join_card
+deck_3.cards << join_card
